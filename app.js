402095184728f6db59c690673290b0d1e75ef2fa
abdul qadir <aq348025@gmail.com>
2023-06-01 02:12:32 +0500

    some work add and remove

--- app.js
+++ app.js
@@ -1,60 +1,67 @@
-var BudgetAmount =document.getElementById("BudgetAmount")
-var AddBudget = document.getElementById("AddBudget")
-var ExpenseDescription = document.getElementById("ExpenseDescription")
-var ExpenseAmount = document.getElementById("ExpenseAmount")
-var dates = document.getElementById("dates")
-var AddExpense = document.getElementById("AddExpense")
-var budget1 = document.getElementById("Bud-get")
-var expense1 = document.getElementById("Expense")
-var Balance = document.getElementById("Balance")
-var des = document.getElementById("des")
-var exp = document.getElementById("exp")
-var dat = document.getElementById("dat")
-var inputText = 0
-AddBudget.addEventListener("click", function(){
-   inputText = BudgetAmount.value
+var BudgetAmount =document.getElementById("BudgetAmount");
+var AddBudget = document.getElementById("AddBudget");
+var ExpenseDescription = document.getElementById("ExpenseDescription");
+var ExpenseAmount = document.getElementById("ExpenseAmount");
+var dates = document.getElementById("dates");
+var AddExpense = document.getElementById("AddExpense");
+var budget1 = document.getElementById("Bud-get");
+var expense1 = document.getElementById("Expense");
+var Balance = document.getElementById("Balance");
+var des = document.getElementById("des");
+var exp = document.getElementById("exp");
+var dat = document.getElementById("dat");
+var inputText = 0;
+function greet1(){
+   inputText = BudgetAmount.value;
  if(inputText === "" || inputText < 0){
+   
   
 
  }else{
-    budget1.innerHTML = inputText
-    Balance.innerText = inputText - expense1.innerText
-    inputText = ""
+    budget1.innerHTML = inputText;
+    Balance.innerText = inputText - expense1.innerText;
+    inputText = "";
 
  }
   
 //console.log(inputText)
 
   
-})
-var inputText2 = 0
-var inputText3 = 0
-var inputText4 = 0
-AddExpense.addEventListener("click", function(){
-   inputText2 = ExpenseAmount.value 
-   inputText3 =  ExpenseDescription.value
-   inputText4 = dates.value
+}
+var inputText2 = 0;
+var inputText3 = 0;
+var inputText4 = 0;
+var saved = [];
+function greet(){
+   var saves = {
+   expense: inputText2 = ExpenseAmount.value ,
+   description: inputText3 =  ExpenseDescription.value,
+   date: inputText4 = dates.value,
+}
    if(inputText2 === "" || inputText2 < 0 ){
 
    }else{
-      expense1.innerText = inputText2
-      exp.innerText = inputText2
-      inputText2 = ""
+      expense1.innerText = inputText2;
+      exp.innerText = inputText2;
+      inputText2 = "";
       
    }
    if(inputText3 === "" || inputText3 < 0){
 
    }else{
-      des.innerText = inputText3
+      des.innerText = inputText3;
    
    }if (inputText4 === "" || inputText4 < 0){
 
    }else{
-      dat.innerText = inputText4
+      dat.innerText = inputText4;
    }
-   
+      saved.push(saves);
+   let sringify = JSON.stringify(saved);
+   localStorage.setItem("saved", sringify);
+//   console.log(saved);
 
-})
+}
 
 
 
